4513097dff721750eebb6cef4ce87d9de7e509c8
aaadi02 <adityadakhane011@gmail.com>
2024-10-03 14:50:17 +0530

    signup flow completed

--- controller/authController.js
+++ controller/authController.js
@@ -1,10 +1,45 @@
-const signup = (req, res, next) => {
+const userModel = require("../model/userSchema");
+const emailValidator = require('email-validator');
+
+const signup = async(req, res, next) => {
   const {name, email, password, confirmPassword} = req.body;
   console.log(name, email, password, confirmPassword);
-  return res.status(200).json({
-    sucess: true,
-    data: {}
+
+  if(!name || !email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: 'Every field is mandatory !'
+    })
+  }
+
+  const validEmail = emailValidator.valid(email);
+  if(!validEmail) {
+    return res.status(400).json({
+      success: false,
+      message: 'please enter valid email id !'
+    })
+  }
+
+  try {
+    const userInfo = userModel(req.body);
+    const result = await userInfo.save();
+
+    return res.status(200).json({
+      sucess: true,
+      data: result
   })
+  }catch(e) {
+    if(e.code === 11000) {
+      return res.status(400).json({
+        success: false,
+        message: 'account already exists with provided email id'
+      })
+    }
+    return res.status(400).json({
+      success: false,
+      message: e.message
+    })
+  }
 };
 
 module.exports = {signup};
